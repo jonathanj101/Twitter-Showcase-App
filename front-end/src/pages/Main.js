e2e6829b67f3b62e652324b0b3630a9f8aa80dca
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 02:50:21 -0500

    Main component, will handle most front end rendering
    Main component, will handle most front end rendering

---
+++ front-end/src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
+import Footer from '../components/Footer'
+import Home from '../pages/Home'
+
+const Main = () => {
+    return (
+        <div>
+            <NavbarComponent />
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main
