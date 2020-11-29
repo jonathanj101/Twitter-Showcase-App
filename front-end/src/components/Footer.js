1c05b5a0abd947ffb5c855a3237edd5b418db04a
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 02:48:12 -0500

    Footer component created
    Footer component created

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-dark h-100">
+            <div className="center-info">
+                <p className=" text-white">@Jonathan J || Full Stack Developer</p>
+            </div>
+            <div className="website-info">
+                <a
+                    className="s"
+                    href="https://www.linkedin.com/feed/">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
