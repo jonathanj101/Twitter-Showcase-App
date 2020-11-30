863b432a5ec2a145152df53938196a0cc6dc9bf9
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 22:22:02 -0500

    using NavLink instead of Link, as Link no longer support active attributes
    using NavLink instead of Link, as Link no longer support active attributes

--- front-end/src/components/NavbarComponent.js
+++ front-end/src/components/NavbarComponent.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 import twitterBirdIcon from '../images/twitter-bird-icon.jpg'
 
 const NavbarComponent = () => {
     return (
         <div>
-            <Navbar className="d-flex justify-content-between" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">
+            <Navbar
+                className="d-flex justify-content-between"
+
+                bg="dark"
+                variant="dark">
+                <Navbar.Brand>
                     <img
                         alt="twitter bird"
                         src={twitterBirdIcon}
@@ -17,25 +22,29 @@ const NavbarComponent = () => {
                 </Navbar.Brand>
                 <Nav
                     variant='pills'
-                    className="">
-                    <Nav.Link
-                        href="#home"
-                        className="mr-3"
+                    className=""
+                >
+                    <NavLink
+                        to="/"
+                        exact
+                        className="nav-link mr-3"
                     >
                         Home
-                    </Nav.Link>
-                    <Nav.Link
-                        href="#search"
-                        className="mr-3"
+                    </NavLink>
+                    <NavLink
+                        to="/search"
+                        exact
+                        className="nav-link mr-3"
                     >
                         Search
-                    </Nav.Link>
-                    <Nav.Link
-                        href="#random"
-                        className=""
+                    </NavLink>
+                    <NavLink
+                        to="/random"
+                        exact
+                        className="nav-link "
                     >
                         Random
-                    </Nav.Link>
+                    </NavLink>
                 </Nav>
             </Navbar>
         </div>
