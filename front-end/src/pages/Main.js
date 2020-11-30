64515d536f005d2afc44beaafe6d1c2206742ad0
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 22:23:21 -0500

    added and implemented Switch from router dom
    added and implemented Switch from router dom

--- front-end/src/pages/Main.js
+++ front-end/src/pages/Main.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import NavbarComponent from '../components/NavbarComponent'
 import Footer from '../components/Footer'
 import Home from '../pages/Home'
+import SearchComponent from './Search'
 
 const Main = () => {
     return (
         <div>
             <NavbarComponent />
-            <Home />
+            <Switch >
+                <Route path="/" exact component={() => <Home />} />
+                <Route path='/search' exact component={() => <SearchComponent />} />
+            </Switch>
             <Footer />
         </div>
     )
