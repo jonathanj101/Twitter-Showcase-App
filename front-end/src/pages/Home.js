7b10091ae9352f8fb9c4113e4546048c3a2ac150
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 22:22:47 -0500

    added some styles,
    added some styles,

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { Toast } from 'react-bootstrap'
-import stickManFishing from '../images/stickman-fishing.jpg'
+// import stickManFishing from '../images/stickman-fishing.jpg'
+import fishingTweets from '../images/fishing-tweet.jpg'
 
 const Home = () => {
     return (
         <div className='bg-dark'>
             <div className="container">
-                <Toast>
+                <Toast className='mx-auto'>
                     <Toast.Header>
-                        <img src={stickManFishing} className="stick rounded mr-2" alt="" />
+                        <img src={fishingTweets} className="stick rounded mr-2" alt="" />
                         <strong className="mr-auto">Fishing Tweets!</strong>
                     </Toast.Header>
-                    <Toast.Body>Click on Search to look up Tweets from Twitter</Toast.Body>
-                    <Toast.Body>Don't know what to look? Click on Random to see selected Twitter's users tweets!</Toast.Body>
+                    <Toast.Body>
+                        * This application uses Twitter API to search desired user's tweets
+                    </Toast.Body>
+                    <Toast.Body>
+                        * Click on Search, to search for your favorite person's tweets
+                    </Toast.Body>
+                    <Toast.Body>
+                        * Don't know what to look? Click on Random to see selected Twitter's users tweets!
+                    </Toast.Body>
+                    <Toast.Body>
+                        * Have Fun!
+                    </Toast.Body>
                 </Toast>
             </div>
         </div>
