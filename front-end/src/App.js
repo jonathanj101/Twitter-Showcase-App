a1ee8a8897dab324214e6e4b755757197220bbaf
jonathanj101 <jonatjimen@gmail.com>
2020-11-29 22:21:18 -0500

    BrowserRouter added and implemented
    BrowserRouter added and implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-// import NavbarComponent from './components/NavbarComponent'
-// import Footer from './components/Footer'
+import { BrowserRouter } from 'react-router-dom'
+
+
+
 import Main from './pages/Main'
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
