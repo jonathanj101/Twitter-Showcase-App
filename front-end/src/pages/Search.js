edca8d540dffd3d4890ade501c36a0687a5536b3
jonathanj101 <jonatjimen@gmail.com>
2020-11-30 20:58:54 -0500

    trying to use function from parent and then lift state but get an error instead
    trying to use function from parent and then lift state but get an error instead

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchComponent = () => {
+const SearchComponent = (props) => {
+    console.log(props)
+    const [text, getText] = useState('')
+
+    function getValueInput(e) {
+        const { value } = e.target
+        console.log(value)
+        getText(value)
+    }
+
     return (
         <div className="bg-dark">
             <div className="input-group mx-auto w-50">
                 <input
                     className="form-control"
                     type="text"
+                    value={text}
+                    onChange={getValueInput}
                     placeholder="Search for a user tweet..." />
                 <div className="input-group-append">
                     <button
+                        onClick={() => props.getUserText(text)}
                         className="btn btn-outline-secondary bg-dark text-light" type="button">
                         Search
                     </button>
