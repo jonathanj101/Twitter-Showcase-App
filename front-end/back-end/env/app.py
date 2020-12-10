0c8176b7496b9ba0533bb969e2a8a90b86ce9967
jonathanj101 <jonatjimen@gmail.com>
2020-12-10 01:56:15 -0500

    dummy data, testing
    dummy data, testing

--- front-end/back-end/env/app.py
+++ front-end/back-end/env/app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home', methods=['GET'])
 def index():
     return {
-        "lastName": "importa",
+        "lastName": 1,
         "name": "queti",
     }
