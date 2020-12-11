974c1f0cf353d705d7fdfb53bb7c84be191aac76
jonathanj101 <jonatjimen@gmail.com>
2020-12-10 22:17:13 -0500

    installed tweepy for twitter api with pytho interface
    installed tweepy for twitter api with pytho interface

--- back-end/env/app.py
+++ back-end/env/app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import json
+import tweepy
+
+f = open('../twitter-cred.json',)
+data = json.load(f)
+
+# print(data.get("CONSUMER_KEY"))
+CONSUMER_KEY = data.get("CONSUMER_KEY")
+CONSUMER_SECRET = data.get("CONSUMER_SECRET")
+ACCESS_TOKEN = data.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = data.get("ACCESS_TOKEN_SECRET")
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
 
 app = Flask(__name__)
 
 
 @app.route('/home', methods=['GET'])
 def index():
-    return {
-        "lastName": 1,
-        "name": "queti",
-    }
+    return data
