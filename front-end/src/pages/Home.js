5444b96c7faaddf8b7b6873f933b821524eab478
jonathanj101 <jonatjimen@gmail.com>
2020-12-13 21:55:54 -0500

    Toast working as i expected, added a bit of animation for user i
    Toast working as i expected, added a bit of animation for user i

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Toast } from 'react-bootstrap'
+import { Toast, Button } from 'react-bootstrap'
 import fishingTweets from '../images/fishing-tweet.jpg'
 
 const Home = () => {
-    const [initialData, setInitialData] = useState([{}])
+    const [initialData, setInitialData] = useState({})
+    const [showA, setShowA] = useState(false);
+
+    const toggleShowA = () => setShowA(!showA);
 
     useEffect(() => {
         axios('/home')
@@ -20,19 +23,27 @@ const Home = () => {
             className='bg-secondary'
             style={divHeight}>
             <div className="container pt-2">
-                <Toast className='mx-auto' style={toastPadding}>
-                    <Toast.Header>
+                <Toast
+                    className='mx-auto mb-0'
+                    style={toastPadding}>
+                    <Toast.Header closeButton={false}>
                         <img src={fishingTweets} className="stick rounded mr-2" alt="" />
                         <strong className="mr-auto">Fishing Tweets!</strong>
+                        <Button onClick={toggleShowA}>Click Me</Button>
                     </Toast.Header>
-                    <Toast.Body>
+                </Toast>
+                <Toast
+                    className="mx-auto"
+                    onClose={toggleShowA}
+                    show={showA}>
+                    <Toast.Body >
                         * This application uses Twitter API to search desired user's tweets
                     </Toast.Body>
                     <Toast.Body>
                         * Click on Search, to search for your favorite person's tweets
                     </Toast.Body>
                     <Toast.Body>
-                        * Don't know what to look? Click on Random to see selected Twitter's users tweets!
+                        * Don't know what to look? Click on Random to see my selected Twitter's users tweets!
                     </Toast.Body>
                     <Toast.Body>
                         * Have fun
@@ -42,6 +53,7 @@ const Home = () => {
         </div>
     )
 }
+
 var divHeight = {
     height: '75.25vh',
     minHeight: '75.25vh'
