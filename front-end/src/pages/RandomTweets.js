fa1e7f3e0243c57726a6fbcb82f1c183e49824a1
jonathanj101 <jonatjimen@gmail.com>
2020-12-16 01:17:13 -0500

    toast rendering bug fixed
    toast rendering bug fixed

--- front-end/src/pages/RandomTweets.js
+++ front-end/src/pages/RandomTweets.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Toast, Button } from 'react-bootstrap'
+import { Toast, Button, Image } from 'react-bootstrap'
 import redBlueTweet from '../images/red-blue-tweet.jpg'
 
 const RandomTweetsComponent = () => {
@@ -8,29 +8,74 @@ const RandomTweetsComponent = () => {
     const toggleShowA = () => setShowA(!showA);
     return (
         <div
-            className="bg-secondary"
+            className="bg-secondary d-flex align-items-center "
             style={divHeight}>
-            <div className="">
-                <Toast className="mb-0 w-50">
+            <div className=" d-flex justify-content-between">
+                <Toast className="w-25 h-50">
                     <Toast.Header closeButton={false}>
-                        <img
-                            className="stick rounded mr-2"
+                        <Image
+                            className="stick mr-2"
                             src={redBlueTweet}
-                            alt="twiiter alike logo" />
+                            alt="twiiter alike logo"
+                            roundedCircle />
+                        <strong className="mr-auto">Bootstrap</strong>
+                        <strong style={userNameStyle}>username</strong>
+                        <small>created at</small>
+                        <Button onClick={toggleShowA}>click me</Button>
+                    </Toast.Header>
+                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    </Toast>
+                </Toast>
+                <Toast className="w-25 h-50">
+                    <Toast.Header closeButton={false}>
+                        <Image
+                            className="stick mr-2"
+                            src={redBlueTweet}
+                            alt="twiiter alike logo"
+                            roundedCircle />
                         <strong className="mr-auto">Bootstrap</strong>
                         <strong style={userNameStyle}>username</strong>
                         <small>11 mins ago</small>
                         <Button onClick={toggleShowA}>click me</Button>
                     </Toast.Header>
+                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    </Toast>
                 </Toast>
-                <Toast className="w-50" onClose={toggleShowA} show={showA}>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                <Toast className="w-25 h-50">
+                    <Toast.Header closeButton={false}>
+                        <Image
+                            className="stick mr-2"
+                            src={redBlueTweet}
+                            alt="twiiter alike logo"
+                            roundedCircle />
+                        <strong className="mr-auto">Bootstrap</strong>
+                        <strong style={userNameStyle}>username</strong>
+                        <small>11 mins ago</small>
+                        <Button onClick={toggleShowA}>click me</Button>
+                    </Toast.Header>
+                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    </Toast>
                 </Toast>
             </div>
         </div>
@@ -44,39 +89,9 @@ var divHeight = {
 var userNameStyle = {
     position: "relative",
     top: "25px",
-    right: "17.5rem",
+    right: "32rem",
 }
 
-var toastPadding = {
-    margin: "10% auto"
-}
+
 
 export default RandomTweetsComponent
-
-
-{/* <div className="card-group">
-<div className="card" >
-    <img src={idk} class="card-img-top" alt="..." />
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">idk</p>
-        <button className="btn btn-primary">Go somewhere</button>
-    </div>
-</div>
-<div className="card" >
-    <img src={idk} class="card-img-top" alt="..." />
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">idk</p>
-        <button className="btn btn-primary">Go somewhere</button>
-    </div>
-</div>
-<div className="card" >
-    <img src={idk} class="card-img-top" alt="..." />
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">idk</p>
-        <button className="btn btn-primary">Go somewhere</button>
-    </div>
-</div>
-</div> */}
