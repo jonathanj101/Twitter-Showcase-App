4f57016ff00e138997cdd4d83618799db1985727
jonathanj101 <jonatjimen@gmail.com>
2020-12-16 23:04:44 -0500

    http request send and recieved correctly, working as expected
    http request send and recieved correctly, working as expected

--- front-end/src/pages/RandomTweets.js
+++ front-end/src/pages/RandomTweets.js
@@ -1,47 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Toast, Button, Image } from 'react-bootstrap'
 import redBlueTweet from '../images/red-blue-tweet.jpg'
 
 const RandomTweetsComponent = () => {
+    const [andysTweets, setAndysTweet] = useState({})
     const [showA, setShowA] = useState(false);
+    const [showB, setShowB] = useState(false);
+    const [showC, setShowC] = useState(false);
 
     const toggleShowA = () => setShowA(!showA);
+    const toggleShowB = () => setShowB(!showB);
+    const toggleShowC = () => setShowC(!showC);
+
+    useEffect(() => {
+        axios('/home')
+            .then(response => {
+                setAndysTweet(response.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+    console.log(andysTweets)
+
     return (
         <div
-            className="bg-secondary d-flex align-items-center "
+            className="bg-secondary d-flex justify-content-between"
             style={divHeight}>
-            <div className=" d-flex justify-content-between">
-                <Toast className="w-25 h-50">
+            <div className="w-50">
+                <Toast>
                     <Toast.Header closeButton={false}>
                         <Image
                             className="stick mr-2"
                             src={redBlueTweet}
                             alt="twiiter alike logo"
                             roundedCircle />
-                        <strong className="mr-auto">Bootstrap</strong>
-                        <strong style={userNameStyle}>username</strong>
-                        <small>created at</small>
-                        <Button onClick={toggleShowA}>click me</Button>
-                    </Toast.Header>
-                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    </Toast>
-                </Toast>
-                <Toast className="w-25 h-50">
-                    <Toast.Header closeButton={false}>
-                        <Image
-                            className="stick mr-2"
-                            src={redBlueTweet}
-                            alt="twiiter alike logo"
-                            roundedCircle />
-                        <strong className="mr-auto">Bootstrap</strong>
-                        <strong style={userNameStyle}>username</strong>
+                        <strong className="mr-auto">{andysTweets.name}</strong>
+                        <small style={userNameStyle}>{andysTweets.username}</small>
                         <small>11 mins ago</small>
                         <Button onClick={toggleShowA}>click me</Button>
                     </Toast.Header>
@@ -55,7 +49,10 @@ const RandomTweetsComponent = () => {
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                     </Toast>
                 </Toast>
-                <Toast className="w-25 h-50">
+
+            </div>
+            <div className="w-50">
+                <Toast>
                     <Toast.Header closeButton={false}>
                         <Image
                             className="stick mr-2"
@@ -65,9 +62,33 @@ const RandomTweetsComponent = () => {
                         <strong className="mr-auto">Bootstrap</strong>
                         <strong style={userNameStyle}>username</strong>
                         <small>11 mins ago</small>
-                        <Button onClick={toggleShowA}>click me</Button>
+                        <Button onClick={toggleShowB}>click me</Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
+                    <Toast className="w-100" onClose={toggleShowB} show={showB}>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    </Toast>
+                </Toast>
+            </div>
+            <div className="w-50">
+                <Toast>
+                    <Toast.Header closeButton={false}>
+                        <Image
+                            className="stick mr-2"
+                            src={redBlueTweet}
+                            alt="twiiter alike logo"
+                            roundedCircle />
+                        <strong className="mr-auto">Bootstrap</strong>
+                        <strong style={userNameStyle}>username</strong>
+                        <small>11 mins ago</small>
+                        <Button onClick={toggleShowC}>click me</Button>
+                    </Toast.Header>
+                    <Toast className="w-100" onClose={toggleShowC} show={showC}>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
@@ -81,6 +102,7 @@ const RandomTweetsComponent = () => {
         </div>
     )
 }
+
 var divHeight = {
     height: '75.25vh',
     minHeight: '75.25vh'
@@ -89,9 +111,7 @@ var divHeight = {
 var userNameStyle = {
     position: "relative",
     top: "25px",
-    right: "32rem",
+    right: "16rem",
 }
-
-
 
 export default RandomTweetsComponent
