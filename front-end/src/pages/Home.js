e27a4ac89bf5de167c50f18f7a313e608659f56e
jonathanj101 <jonatjimen@gmail.com>
2020-12-16 23:04:22 -0500

    moved HTTP requests to randomTweets component, where it belong
    moved HTTP requests to randomTweets component, where it belong

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { Toast, Button } from 'react-bootstrap'
 import fishingTweets from '../images/fishing-tweet.jpg'
 
 const Home = () => {
-    const [initialData, setInitialData] = useState({})
     const [showA, setShowA] = useState(false);
 
     const toggleShowA = () => setShowA(!showA);
 
-    useEffect(() => {
-        axios('/home')
-            .then(response => {
-                setInitialData(response.data)
-            })
-            .catch(err => console.log(err))
-    }, [])
-    console.log(initialData)
 
     return (
         <div
