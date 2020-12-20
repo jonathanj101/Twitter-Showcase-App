f681655e9627bc98b7a604b138441a2f24fdcdfe
jonathanj101 <jonatjimen@gmail.com>
2020-12-20 02:31:47 -0500

    testing
    testing

--- back-end/env/app.py
+++ back-end/env/app.py
@@ -17,8 +17,8 @@ api = tweepy.API(auth)
 app = Flask(__name__)
 
 
-@app.route('/randomTweets', methods=['GET'])
-def index():
+@app.route('/randomtweets', methods=['GET'])
+def andy():
     andys = api.get_user(screen_name='andysterks')
     gtrs = api.get_user(screen_name='JustGTRs')
     bmws = api.get_user(screen_name='BMW')
@@ -39,7 +39,7 @@ def index():
     for tweets in bmws_info:
         gtrs_tweets_list.append(tweets.text)
 
-    users_info = {
+    users_info = [{
         "name": {
             "andy": andys.name,
             "gtr": gtrs.name,
@@ -60,9 +60,35 @@ def index():
             "gtrs": gtrs.profile_image_url_https,
             "bmws": bmws.profile_image_url_https
         }
-    }
+    }]
 
     return jsonify(users_info)
+
+
+# @app.route('/gtr', methods=["GET"])
+# def gtr():
+#     gtrs = api.get_user(screen_name='JustGTRs')
+
+#     gtrs_info = api.user_timeline(screen_name='JustGTRs', count=5)
+
+#     gtrs_tweets_list = []
+
+#     for tweets in gtrs_info:
+#         gtrs_tweets_list.append(tweets.text)
+
+#     user_info = {
+#         "name": gtrs.name,
+#         "tweets": gtrs_tweets_list,
+#         "username": gtrs.screen_name,
+#         "profile_image": gtrs.profile_image_url_https
+#     }
+
+#     return jsonify(user_info)
+
+
+# @app.route('/bmw', methods=["GET"])
+# def gtr():
+#     return
 
 
 @app.route('/search/:<name>', methods=['GET'])
