c4c48ff94ca5f248aabd2617aea3d9dd76d0617b
jonathanj101 <jonatjimen@gmail.com>
2020-12-19 21:08:55 -0500

    requests for 3 twitter users, their name username and profile image and tweets
    requests for 3 twitter users, their name username and profile image and tweets

--- back-end/env/app.py
+++ back-end/env/app.py
@@ -14,56 +14,55 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_token)
 auth.set_access_token(access_token_key, access_token_key_secret)
 api = tweepy.API(auth)
 
-
-# f = open('../twitter-creds.json')
-# data = json.load(f)
-
-# print(data.get("CONSUMER_KEY"))
-# CONSUMER_KEY = data.get("CONSUMER_KEY")
-# CONSUMER_SECRET = data.get("CONSUMER_SECRET")
-# ACCESS_TOKEN = data.get("ACCESS_TOKEN")
-# ACCESS_TOKEN_SECRET = data.get("ACCESS_TOKEN_SECRET")
-
-
-# print(dir(user.screen_name))
-# print(" user name {}".format(user.screen_name))
-# print("user count {}".format(user.followers_count))
-# for friend in user.friends():
-#     print("friend screen name {}".format(friend.screen_name))
-
-# public_tweets = api.home_timeline()
-
-# user = api.user_timeline(screen_name='andysterks')
-# user = api.get_user("andysterks")
-# print(dir(user))
-# print(user.ids)
-# print(user)
-# id = 61994400
-# user1 = api.get_user(id=id)
-# print(dir(user1))
-# print(user1.name)
-
-
 app = Flask(__name__)
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/randomTweets', methods=['GET'])
 def index():
-    user_name = api.get_user(screen_name="andysterks")
-    user_time_line = api.user_timeline(screen_name='andysterks', count=5)
+    andys = api.get_user(screen_name='andysterks')
+    gtrs = api.get_user(screen_name='JustGTRs')
+    bmws = api.get_user(screen_name='BMW')
+    andys_info = api.user_timeline(screen_name='andysterks', count=5)
+    gtrs_info = api.user_timeline(screen_name='JustGTRs', count=5)
+    bmws_info = api.user_timeline(screen_name='BMW', count=5)
 
-    tweets_list = []
+    andys_tweets_list = []
+    gtrs_tweets_list = []
+    bmws_tweets_list = []
 
-    for tweets in user_time_line:
-        tweets_list.append(tweets.text)
+    for tweets in andys_info:
+        andys_tweets_list.append(tweets.text)
 
-    new_user = {
-        "name": user_name.name,
-        "username": user_name.screen_name,
-        "tweets": tweets_list
+    for tweets in gtrs_info:
+        gtrs_tweets_list.append(tweets.text)
+
+    for tweets in bmws_info:
+        gtrs_tweets_list.append(tweets.text)
+
+    users_info = {
+        "name": {
+            "andy": andys.name,
+            "gtr": gtrs.name,
+            "bmw": bmws.name
+        },
+        "username": {
+            "andys": andys.screen_name,
+            "gtrs": gtrs.screen_name,
+            "bmws": bmws.screen_name
+        },
+        "tweets": [
+            [andys_tweets_list],
+            [gtrs_tweets_list],
+            [bmws_tweets_list]
+        ],
+        "profile_image": {
+            "andys": andys.profile_image_url_https,
+            "gtrs": gtrs.profile_image_url_https,
+            "bmws": bmws.profile_image_url_https
+        }
     }
 
-    return jsonify(new_user)
+    return jsonify(users_info)
 
 
 @app.route('/search/:<name>', methods=['GET'])
