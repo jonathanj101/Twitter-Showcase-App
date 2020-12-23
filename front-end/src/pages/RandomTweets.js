85817533e41e24776df2117d9c92417c1d0cbf23
jonathanj101 <jonatjimen@gmail.com>
2020-12-23 00:53:41 -0500

    can't work around React Hook useEffect
    can't work around React Hook useEffect. Will need to make request on Main component then pass down data as props to this component

--- front-end/src/pages/RandomTweets.js
+++ front-end/src/pages/RandomTweets.js
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Toast, Button, Image } from 'react-bootstrap'
 
-const RandomTweetsComponent = () => {
-    const [user, setUserData] = useState([{ name: '' }])
-    const [showA, setShowA] = useState(false);
-    const [showB, setShowB] = useState(false);
-    const [showC, setShowC] = useState(false);
+const RandomTweetsComponent = ({ usersData }) => {
+    const [fetchAndysTweets, andysTweets] = useState([])
+    const [fetchBMWSTweets, bmwsTweets] = useState([])
+    const [fetchGTRSTweets, gtrsTweets] = useState([])
 
-    const toggleShowA = () => setShowA(!showA);
-    const toggleShowB = () => setShowB(!showB);
-    const toggleShowC = () => setShowC(!showC);
+    // useEffect(() => {
+    //     fetch('/andy')
+    //         .then(response => response.json())
+    //         .then(tweet => {
+    //             console.log(tweet)
+    //             andysTweets(tweet)
 
-    useEffect(() => {
-        fetch('/randomtweets')
-            .then(response => response.json()).then(data => {
-                setUserData(data)
-            })
-    }, [])
-    console.log(user)
+    //         })
+    // }, [])
+
+    // const showAndysTweets = () => {
+
+    // }
+
+    const [showAndys, setShowAndys] = useState(false);
+    const [showBMWS, setShowBMWS] = useState(false);
+    const [showGTRS, setShowGTRS] = useState(false);
+
+    const toggleAndysTweets = () => setShowAndys(!showAndys);
+    const toggleBMWSTweets = () => setShowBMWS(!showBMWS);
+    const toggleGTRSTweets = () => setShowGTRS(!showGTRS);
 
     return (
         <div
@@ -29,14 +38,40 @@ const RandomTweetsComponent = () => {
                     <Toast.Header closeButton={false}>
                         <Image
                             className="stick mr-2"
-                            // src={user.profile_image.andys}
+                            src={usersData.profile_image.andys}
                             alt="twiiter alike logo"
                             roundedCircle />
-                        {/* <strong className="mr-auto">{user.name.andy}</strong> */}
-                        {/* <small style={userNameStyle}>@{user.username.andy}</small> */}
-                        <Button onClick={toggleShowA}>click me</Button>
+                        <strong className="mr-auto">{usersData.name.andy}</strong>
+                        <small style={userNameStyle}>@{usersData.username.andy}</small>
+                        <Button onClick={toggleAndysTweets}>click me</Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleShowA} show={showA}>
+                    <Toast className="w-100" onClose={toggleAndysTweets} show={showAndys}>
+                        <Button  >andy</Button>
+                        <Toast.Body>
+                            <small>Created at: </small>
+                        </Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    </Toast>
+                </Toast>
+            </div>
+            <div className="w-25">
+                <Toast>
+                    <Toast.Header closeButton={false}>
+                        <Image
+                            className="stick mr-2"
+                            src={usersData.profile_image.gtrs}
+                            alt="twiiter alike logo"
+                            roundedCircle />
+                        <strong className="mr-auto">{usersData.name.gtr}</strong>
+                        <small style={userNameStyle}>@{usersData.username.gtrs}</small>
+                        <Button onClick={toggleBMWSTweets}>click me</Button>
+                    </Toast.Header>
+                    <Toast className="w-100" onClose={toggleBMWSTweets} show={showBMWS}>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
@@ -52,37 +87,14 @@ const RandomTweetsComponent = () => {
                     <Toast.Header closeButton={false}>
                         <Image
                             className="stick mr-2"
-                            // src={user.profile_image}
+                            src={usersData.profile_image.bmws}
                             alt="twiiter alike logo"
                             roundedCircle />
-                        {/* <strong className="mr-auto">{user.name}</strong> */}
-                        {/* <small style={userNameStyle}>@{user.username}</small> */}
-                        <Button onClick={toggleShowB}>click me</Button>
+                        <strong className="mr-auto">{usersData.name.bmw}</strong>
+                        <small style={userNameStyle}>@{usersData.username.bmw}</small>
+                        <Button onClick={toggleGTRSTweets}>click me</Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleShowB} show={showB}>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                    </Toast>
-                </Toast>
-            </div>
-            <div className="w-25">
-                <Toast>
-                    <Toast.Header closeButton={false}>
-                        <Image
-                            className="stick mr-2"
-                            // src={user.profile_image.bmws}
-                            alt="twiiter alike logo"
-                            roundedCircle />
-                        {/* <strong className="mr-auto">{user.name.bmw}</strong> */}
-                        {/* <small style={userNameStyle}>@{user.username.bmw}</small> */}
-                        <Button onClick={toggleShowC}>click me</Button>
-                    </Toast.Header>
-                    <Toast className="w-100" onClose={toggleShowC} show={showC}>
+                    <Toast className="w-100" onClose={toggleGTRSTweets} show={showGTRS}>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
                         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
