74c4679a8989b0243fca5cfda51a45970a3ed365
jonathanj101 <jonatjimen@gmail.com>
2020-12-25 22:18:32 -0500

    renaming variables and functions
    renaming variables and functions, for better readability purpose

--- front-end/src/pages/RandomTweets.js
+++ front-end/src/pages/RandomTweets.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 import { Toast, Button, Image } from 'react-bootstrap'
 
 const RandomTweetsComponent = ({ usersData, andyTweets, bmwTweets, gtrTweets }) => {
-    const [fetchAndysTweets, andysTweets] = useState([])
-    const [fetchBMWSTweets, bmwsTweets] = useState([])
-    const [fetchGTRSTweets, gtrsTweets] = useState([])
+    const [andysRandomTweets, andysTweetsFunc] = useState([])
+    const [fetchBMWSTweets, bmwsTweetsFunc] = useState([])
+    const [fetchGTRSTweets, gtrsTweetsFunc] = useState([])
 
     const [showAndys, setShowAndys] = useState(false);
     const [showBMWS, setShowBMWS] = useState(false);
@@ -14,6 +14,25 @@ const RandomTweetsComponent = ({ usersData, andyTweets, bmwTweets, gtrTweets }) 
     const toggleAndysTweets = () => setShowAndys(!showAndys);
     const toggleBMWSTweets = () => setShowBMWS(!showBMWS);
     const toggleGTRSTweets = () => setShowGTRS(!showGTRS);
+
+
+    const randomNum = (userTweets) => {
+        const randomNum = Math.floor(Math.random() * (userTweets.length))
+        return randomNum
+    }
+
+    const tweetsData = (tweetInfo) => {
+        const tweetNum = randomNum(tweetInfo)
+        if (tweetInfo === andyTweets) {
+            return andysTweetsFunc(tweetInfo[tweetNum])
+        }
+        else if (tweetInfo === bmwTweets) {
+            return bmwsTweetsFunc(tweetInfo[tweetNum])
+        }
+        else {
+            return gtrsTweetsFunc(tweetInfo[tweetNum])
+        }
+    }
 
     return (
         <div
@@ -31,19 +50,22 @@ const RandomTweetsComponent = ({ usersData, andyTweets, bmwTweets, gtrTweets }) 
                             <strong >{usersData.name.andy}</strong>
                             <small >@{usersData.username.andys}</small>
                         </div>
-                        <Button onClick={toggleAndysTweets}>click me</Button>
+                        <Button
+                            onClick={toggleAndysTweets}>
+                            click me
+                        </Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleAndysTweets} show={showAndys}>
-                        <Button  >andy</Button>
-                        <Toast.Body>
-                            <small>Created at: </small>
-                        </Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    <Toast
+                        className="w-100"
+                        onClose={toggleAndysTweets}
+                        show={showAndys}>
+                        <Button
+                            onClick={() => tweetsData(andyTweets)}
+                            className="mx-auto"
+                        >
+                            Random Andy's Tweets
+                        </Button>
+                        <Toast.Body>{andysRandomTweets}</Toast.Body>
                     </Toast>
                 </Toast>
             </div>
@@ -59,16 +81,23 @@ const RandomTweetsComponent = ({ usersData, andyTweets, bmwTweets, gtrTweets }) 
                             <strong className="mr-auto">{usersData.name.gtr}</strong>
                             <small>@{usersData.username.gtrs}</small>
                         </div>
-                        <Button onClick={toggleBMWSTweets}>click me</Button>
+                        <Button
+                            onClick={toggleGTRSTweets}>
+                            Show Tweets
+                        </Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleBMWSTweets} show={showBMWS}>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    <Toast
+                        className="w-100"
+                        onClose={toggleBMWSTweets}
+                        show={showGTRS}>
+                        <Button
+                            onClick={() => tweetsData(gtrTweets)}
+                        >
+                            Random GTR's Tweets
+                        </Button>
+                        <Toast.Body>
+                            {fetchGTRSTweets}
+                        </Toast.Body>
                     </Toast>
                 </Toast>
             </div>
@@ -84,16 +113,24 @@ const RandomTweetsComponent = ({ usersData, andyTweets, bmwTweets, gtrTweets }) 
                             <strong className="mr-auto">{usersData.name.bmw}</strong>
                             <small>@{usersData.username.bmws}</small>
                         </div>
-                        <Button onClick={toggleGTRSTweets}>click me</Button>
+                        <Button
+                            onClick={toggleBMWSTweets}
+                        >
+                            Show Tweets
+                        </Button>
                     </Toast.Header>
-                    <Toast className="w-100" onClose={toggleGTRSTweets} show={showGTRS}>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-                        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                    <Toast
+                        className="w-100"
+                        onClose={toggleBMWSTweets}
+                        show={showBMWS}>
+                        <Button
+                            onClick={() => tweetsData(bmwTweets)}
+                        >
+                            Random BMW's Tweets
+                        </Button>
+                        <Toast.Body>
+                            {fetchBMWSTweets}
+                        </Toast.Body>
                     </Toast>
                 </Toast>
             </div>
