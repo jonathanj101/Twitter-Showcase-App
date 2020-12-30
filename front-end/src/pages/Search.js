7880ec0ac5f32c7ccc8b32c12c4e8cb378739596
jonathanj101 <jonatjimen@gmail.com>
2020-12-29 21:03:05 -0500

    same issue

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -3,6 +3,12 @@ import { Button } from 'react-bootstrap'
 import UserCard from '../components/UserCard'
 
 const SearchComponent = ({ getUserText, searchText, searchedUser }) => {
+    const [name, setName] = useState('')
+    const [username, setUserName] = useState('')
+    const [userProfileImg, setUserProfileImg] = useState('')
+    const [followersCount, setFollowersCount] = useState('')
+    const [friendsCount, setFriendsCount] = useState('')
+    const [userTweets, setUserTweets] = useState([])
     const [text, getText] = useState('')
 
     function clearState() {
@@ -13,6 +19,22 @@ const SearchComponent = ({ getUserText, searchText, searchedUser }) => {
         const { value } = e.target
         console.log(value)
         getText(value)
+    }
+
+    function requestForSearch(user) {
+        console.log(user)
+        if (user !== '') {
+            console.log(user.name)
+            // setName(user.name)
+            // setUserName(user.username)
+            // setUserProfileImg(user.profile_image)
+            // setFollowersCount(user.followers_count)
+            // setFriendsCount(user.following)
+            // setUserTweets(user.tweets)
+        }
+        else {
+            return <div></div>
+        }
     }
 
     return (
@@ -30,6 +52,7 @@ const SearchComponent = ({ getUserText, searchText, searchedUser }) => {
                     <Button
                         onClick={() => {
                             getUserText(text)
+                            requestForSearch(searchedUser)
                             clearState()
                         }}
                         className="btn btn-outline-secondary bg-primary text-light" type="button">
@@ -37,7 +60,8 @@ const SearchComponent = ({ getUserText, searchText, searchedUser }) => {
                     </Button>
                 </div>
             </div>
-            {searchText !== '' ? <UserCard searchText={searchText} searchedUser={searchedUser} /> : <div></div>}
+            {searchText !== '' ? <UserCard searchText={searchText} searchedUser={searchedUser} name={name} username={username} userProfileImg={userProfileImg} followersCount={followersCount} friendsCount={friendsCount} userTweets={userTweets} /> : <div></div>}
+
         </div>
     )
 }
