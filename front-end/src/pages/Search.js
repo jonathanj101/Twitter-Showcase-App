aa2d5588a3c02ce4660a4a66d2a6c2986327a2e6
jonathanj101 <jonatjimen@gmail.com>
2020-12-30 22:13:19 -0500

    refactoring
    refactoring

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -3,12 +3,6 @@ import { Button } from 'react-bootstrap'
 import UserCard from '../components/UserCard'
 
 const SearchComponent = ({ getUserText, searchText, searchedName, user_name, user_profile_img, user_followers, user_friends, user_tweets }) => {
-    const [name, setName] = useState('')
-    const [username, setUserName] = useState('')
-    const [userProfileImg, setUserProfileImg] = useState('')
-    const [followersCount, setFollowersCount] = useState('')
-    const [friendsCount, setFriendsCount] = useState('')
-    const [userTweets, setUserTweets] = useState([])
     const [text, getText] = useState('')
 
     function clearState() {
@@ -17,25 +11,7 @@ const SearchComponent = ({ getUserText, searchText, searchedName, user_name, use
 
     function getValueInput(e) {
         const { value } = e.target
-        console.log(value)
         getText(value)
-    }
-
-
-    function requestForSearch(user) {
-        console.log(user)
-        if (user !== '') {
-            console.log(user)
-            // setName(user.name)
-            // setUserName(user.username)
-            // setUserProfileImg(user.profile_image)
-            // setFollowersCount(user.followers_count)
-            // setFriendsCount(user.following)
-            // setUserTweets(user.tweets)
-        }
-        else {
-            return <div></div>
-        }
     }
 
     return (
@@ -53,15 +29,25 @@ const SearchComponent = ({ getUserText, searchText, searchedName, user_name, use
                     <Button
                         onClick={() => {
                             getUserText(text)
-                            requestForSearch(searchedName)
-                            // clearState()
+                            clearState()
                         }}
                         className="btn btn-outline-secondary bg-primary text-light" type="button">
                         Search
                     </Button>
                 </div>
             </div>
-            {searchText !== '' ? <UserCard searchText={searchText} name={searchedName} searched_username={user_name} user_profile_img={user_profile_img} user_followers={user_followers} user_friends={user_friends} user_tweets={user_tweets} /> : <div></div>}
+            {searchText !== ''
+                ?
+                <UserCard
+                    searchText={searchText}
+                    name={searchedName}
+                    searched_username={user_name}
+                    user_profile_img={user_profile_img}
+                    user_followers={user_followers}
+                    user_friends={user_friends}
+                    user_tweets={user_tweets} />
+                :
+                <div></div>}
 
         </div>
     )
