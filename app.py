8d3af8b0d5444577390829759a20e06c31f7207c
jonathanj101 <jonatjimen@gmail.com>
2021-01-03 20:57:47 -0500

    handling static folder files, not tested yet
    handling static folder files, not tested yet

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tweepy
 import json
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./front-end/build', static_url_path='/')
 
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_token = os.environ.get('CONSUMER_SECRET')
@@ -13,6 +13,16 @@ access_token_key_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 auth = tweepy.OAuthHandler(consumer_key, consumer_token)
 auth.set_access_token(access_token_key, access_token_key_secret)
 api = tweepy.API(auth)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return app.send_static_file('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 
 @app.route('/randomtweets', methods=['GET'])
@@ -103,6 +113,7 @@ def search_user_request(name):
     }
 
     return jsonify({'user_info': user_data})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
