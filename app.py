b5b2277d17dea04abcf0024a5d28ebff0ede6e04
jonathanj101 <jonatjimen@gmail.com>
2021-01-08 10:54:57 -0500

    testing 2

--- app.py
+++ app.py
@@ -116,4 +116,6 @@ def search_user_request(name):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=5000)
+    # app.run(host='0.0.0.0', debug=False, port=5000)
+    app.run(host='0.0.0.0', debug=False,
+            port=int(os.environ.get("PORT", 5000)))
