bccc7620573f6a7740bf2dc93b3c0377f7c269f5
jonathanj101 <jonatjimen@gmail.com>
2021-01-23 00:31:33 -0500

    refactored, handling error 401 to display to user
    refactored, handling error 401 to display to user

--- front-end/src/components/UserCard.js
+++ front-end/src/components/UserCard.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
 import { Button, Card, Image, Toast } from 'react-bootstrap'
+import Error401 from '../components/Error401'
 
-const UserCard = ({ name, searched_username, user_profile_img, user_followers, user_friends, user_tweets }) => {
+const UserCard = ({
+    name,
+    searched_username,
+    user_profile_img,
+    user_followers,
+    user_friends,
+    user_tweets,
+    is401Var,
+    errMsg }) => {
     const [tweets, setTweets] = useState()
     const [showToast, setToast] = useState(false)
 
@@ -16,44 +25,49 @@ const UserCard = ({ name, searched_username, user_profile_img, user_followers, u
         const tweetNum = randomNum(tweetInfo)
         setTweets(tweetInfo[tweetNum])
     }
+    console.log(is401Var)
 
     return (
         <div>
-            <Card className='card w-25 mx-auto mt-5'>
-                <div className="d-flex flex-row ">
-                    <Image
-                        className='h-25 w-25'
-                        src={user_profile_img}
-                        roundedCircle />
-                    <div className="d-flex justify-content-center flex-column flex-wrap ml-2">
-                        <strong>{name}</strong>
-                        <small>@{searched_username}</small>
+            {!is401Var
+                ?
+                <Card className='card w-25 mx-auto mt-5'>
+                    <div className="d-flex flex-row ">
+                        <Image
+                            className='h-25 w-25'
+                            src={user_profile_img}
+                            roundedCircle />
+                        <div className="d-flex justify-content-center flex-column flex-wrap ml-2">
+                            <strong>{name}</strong>
+                            <small>@{searched_username}</small>
+                        </div>
                     </div>
-                </div>
-                <div className="d-flex flex-row flex-wrap">
-                    <Card.Text className="ml-5">{user_friends} <strong>Following</strong></Card.Text>
-                    <Card.Text className="ml-5">{user_followers} <strong>Followers</strong></Card.Text>
-                </div>
-                <Button
-                    variant="primary"
-                    onClick={() => toggleToast()}>
-                    Show Tweets
-                </Button>
-                <Toast
-                    className="text-center"
-                    onClose={() => toggleToast()}
-                    show={showToast}>
+                    <div className="d-flex flex-row flex-wrap">
+                        <Card.Text className="ml-5">{user_friends} <strong>Following</strong></Card.Text>
+                        <Card.Text className="ml-5">{user_followers} <strong>Followers</strong></Card.Text>
+                    </div>
                     <Button
-                        onClick={() => tweetsData(user_tweets)}
-                        block>
-                        Random Tweets
+                        variant="primary"
+                        onClick={() => toggleToast()}>
+                        Show Tweets
+                </Button>
+                    <Toast
+                        className="text-center"
+                        onClose={() => toggleToast()}
+                        show={showToast}>
+                        <Button
+                            onClick={() => tweetsData(user_tweets)}
+                            block>
+                            Random Tweets
                     </Button>
-                    <Toast.Body>
-                        {tweets}
-                    </Toast.Body>
-                </Toast>
-            </Card>
-        </div>
+                        <Toast.Body>
+                            {tweets}
+                        </Toast.Body>
+                    </Toast>
+                </Card>
+                :
+                <Error401 errorMsg={errMsg} />}
+        </div >
     )
 }
 
